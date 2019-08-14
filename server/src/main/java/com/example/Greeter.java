d6a4e2761581b2399305e5b53f2b78b4c6475518
Antonio Guerrero Torrejon <jantonio774@hotmail.com>
2019-08-14 11:00:36 +0200

    cuarta prueba

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,10 @@ public class Greeter {
 
   }
 
+  /**
+   * @param nombre de la persona
+   * @return greeeting string
+   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
